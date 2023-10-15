c882a93c571d880da9c6d656fe35a6d240e563fb
Farzad Hayat <farzadhayat2002@gmail.com>
2023-10-15 16:01:53 +1000

    picoCTF > General Skills > Magikarp Ground Mission

---
+++ pico/general_skills/magikarp_ground_mission/script.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+# execute the following after connecting with ssh
+cat /home/ctf-player/drop-in/1of3.flag.txt /2of3.flag.txt /home/ctf-player/3of3.flag.txt
+
+# concatenate the three outputs to form the flag
